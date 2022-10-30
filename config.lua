e3203f100c7a9ca748df67a19de1e03922a8ce94
Corey Spohn <cas584@cornell.edu>
2022-10-30 12:31:18 -0400

    Update to remove kj exit of insert mode

--- config.lua
+++ config.lua
@@ -25,6 +25,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.insert_mode["jk"] = false
+lvim.keys.insert_mode["kj"] = false
 
 
 -- unmap a default keymapping
